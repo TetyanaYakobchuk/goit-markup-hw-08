1b60a8e655a08278af192cca8e94ecb2c001c1fa
TetyanaYakobchuk <61588572+TetyanaYakobchuk@users.noreply.github.com>
2021-01-24 13:00:24 +0200

    Add files via upload

---
+++ js/submit.js
@@ -0,0 +1,6 @@
+(() => {
+    document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+      e.preventDefault();
+      new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+    });
+  })();
